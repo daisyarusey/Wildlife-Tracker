e69e09c6078473708149697188b18fc94e055a01
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-15 23:21:21 +0300

    Test for methods save and all()

--- src/test/java/models/SightingTest.java
+++ src/test/java/models/SightingTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -15,6 +16,9 @@ public class SightingTest {
     @After
     public void tearDown() throws Exception {
     }
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
 
     @Test
     public void sighting_instantiatesCorrectly_true() {
@@ -37,5 +41,22 @@ public class SightingTest {
     public void getRangerId_personInstantiatesWithRangerId_String() {
         Sighting testSighting = new Sighting(2,"Zone A",2);
         assertEquals(2, testSighting.getRangerId());
+    }
+
+    @Test
+    public void save_insertsObjectIntoDatabase_Sighting() {
+        Sighting testSighting = new Sighting(2,"Zone A",2);
+        testSighting.save();
+        assertTrue(Sighting.all().get(0).equals(testSighting));
+    }
+
+    @Test
+    public void all_returnsAllInstancesOfSighting_true() {
+        Sighting firstSighting = new Sighting(2,"Zone A",2);
+        firstSighting.save();
+        Sighting secondSighting = new Sighting(2,"Zone A",2);
+        secondSighting.save();
+        assertEquals(true, Sighting.all().get(0).equals(firstSighting));
+        assertEquals(true, Sighting.all().get(1).equals(secondSighting));
     }
 }
