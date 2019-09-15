0502af0c915ca0bbeb3929937b87cd52f03e7f34
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-16 00:02:08 +0300

    Create a test method to test if database assigns id to sighting and also another test to see if we can find a sighting by its id

--- src/test/java/models/SightingTest.java
+++ src/test/java/models/SightingTest.java
@@ -22,41 +22,59 @@ public class SightingTest {
 
     @Test
     public void sighting_instantiatesCorrectly_true() {
-        Sighting testSighting = new Sighting(2,"Zone A",2);
+        Sighting testSighting = new Sighting(2, "Zone A", 2);
         assertEquals(true, testSighting instanceof Sighting);
     }
+
     @Test
     public void getAnimalId_sightingInstantiatesWithAnimalId_2() {
-        Sighting testSighting = new Sighting(2,"Zone A",2);
-        assertEquals(2, testSighting. getAnimalId());
+        Sighting testSighting = new Sighting(2, "Zone A", 2);
+        assertEquals(2, testSighting.getAnimalId());
     }
 
     @Test
     public void getLocation_personInstantiatesWithLocation_String() {
-        Sighting testSighting = new Sighting(2,"Zone A",2);
+        Sighting testSighting = new Sighting(2, "Zone A", 2);
         assertEquals("Zone A", testSighting.getLocation());
     }
 
     @Test
     public void getRangerId_personInstantiatesWithRangerId_String() {
-        Sighting testSighting = new Sighting(2,"Zone A",2);
+        Sighting testSighting = new Sighting(2, "Zone A", 2);
         assertEquals(2, testSighting.getRangerId());
     }
 
     @Test
     public void save_insertsObjectIntoDatabase_Sighting() {
-        Sighting testSighting = new Sighting(2,"Zone A",2);
+        Sighting testSighting = new Sighting(2, "Zone A", 2);
         testSighting.save();
         assertTrue(Sighting.all().get(0).equals(testSighting));
     }
 
     @Test
     public void all_returnsAllInstancesOfSighting_true() {
-        Sighting firstSighting = new Sighting(2,"Zone A",2);
+        Sighting firstSighting = new Sighting(2, "Zone A", 2);
         firstSighting.save();
-        Sighting secondSighting = new Sighting(2,"Zone A",2);
+        Sighting secondSighting = new Sighting(4, "Zone B", 2);
         secondSighting.save();
         assertEquals(true, Sighting.all().get(0).equals(firstSighting));
         assertEquals(true, Sighting.all().get(1).equals(secondSighting));
+    }
+
+    @Test
+    public void save_assignsIdToObject() {
+        Sighting testSighting = new Sighting(2, "Zone A", 2);
+        testSighting.save();
+        Sighting savedSighting = Sighting.all().get(0);
+        assertEquals(testSighting.getId(), savedSighting.getId());
+    }
+
+    @Test
+    public void find_returnsSightingWithSameId_secondSighting() {
+        Sighting firstSighting = new Sighting(2, "Zone A", 2);
+        firstSighting.save();
+        Sighting secondSighting = new Sighting(4, "Zone B", 2);
+        secondSighting.save();
+        assertEquals(Sighting.find(secondSighting.getId()), secondSighting);
     }
 }
