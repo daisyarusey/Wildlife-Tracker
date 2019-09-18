cc3cf91c3922b42c753398adf312aa4dd772e1f2
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-18 15:15:19 +0300

    create Ranger constructor and implement method all to list rangers from table rangers in the database

--- src/main/java/models/Ranger.java
+++ src/main/java/models/Ranger.java
@@ -1,4 +1,42 @@
 package models;
 
-public class Rangers {
+import org.sql2o.Connection;
+
+import java.util.List;
+
+public class Ranger {
+    public int id;
+    public String name;
+    public int budge_number;
+    public int contact;
+
+    public Ranger(int id, String name, int budge_number, int contact) {
+        this.id = id;
+        this.budge_number = budge_number;
+        this.name = name;
+        this.contact = contact;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBudge_number() {
+        return budge_number;
+    }
+
+    public int getContact() {
+        return contact;
+    }
+
+    public static List<Ranger> all() {
+        String sql = "SELECT * FROM rangers ";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Ranger.class);
+        }
+    }
 }
