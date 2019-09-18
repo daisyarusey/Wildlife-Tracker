c5d57de639969ace1eb01a112014451fd13fa7b9
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-18 15:21:46 +0300

    Insert background image

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,38 @@
+import models.Location;
+import models.Ranger;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/",(request, response) ->{
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model,"index.hbs");
+
+        },new HandlebarsTemplateEngine());
+
+        //get list of rangers
+        get("/view/rangers", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("rangers", Ranger.all());
+            return new ModelAndView(model, "ranger-table.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //get list of locations
+        get("/view/locations", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("locations", Location.all());
+            return new ModelAndView(model, "location-list.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+
+
+
 }
