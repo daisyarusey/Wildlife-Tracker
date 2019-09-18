b6fe8f87e8cf2f7021a1b109fc10433eb7195b5b
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-18 15:17:48 +0300

    Create class Locations, contructor locations and implement method all() to list all locations in the location table

--- src/main/java/models/Location.java
+++ src/main/java/models/Location.java
@@ -1,4 +1,38 @@
 package models;
 
+import org.sql2o.Connection;
+
+import java.util.List;
+
 public class Location {
+    private int id;
+    private String name;
+
+    public Location(int id,String name){
+        this.id=id;
+        this.name=name;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public static List<Location> all() {
+        String sql = "SELECT * FROM locations ";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Location.class);
+        }
+    }
 }
